1ddd55cc663763f9e6f159e5188131413ebc090b
sachan12 <sachinnanda427@gmail.com>
2024-03-18 01:25:17 -0700

    add these files;

---
+++ main.js
@@ -0,0 +1,20 @@
+// Loop means repitition or hydration ;
+// Loop have 3 basic types;
+// 1-For-loop   2-while loop   3-do while loop ;
+
+// There are three stages within the loop of (for); 1-initialization 2-condition 3-Increment or Decrement
+// for(var i=0; i < 10; i++){
+//     document.write(i + " ) yarooshah" + "<br>")
+// }
+// for(var i=0; i < 10; i++){
+//     document.write( i  + " ) saylani" +  "<br>");
+// }
+// for (var i=0; i < 10; i++){
+//     document.write(`${i}  ) Sachan  <br>`)
+// }
+
+
+
+// for(var i=1; i<10; i++){
+//     document.write(`${i} ) Karan <br>`);
+// }
